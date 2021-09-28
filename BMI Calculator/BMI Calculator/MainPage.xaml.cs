3aba3f7176b2e721c8c90af8b83fc0e9b40fd7a4
Maciej Kolacinski <mattkaydev@gmail.com>
2021-09-28 12:05:02 +0100

    Initial commit

---
+++ BMI Calculator/BMI Calculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        private void heightText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if (heightText.Text == "")
+            //    HeightSlider.Value = HeightSlider.Minimum;
+            //else
+            //    HeightSlider.Value = Convert.ToInt32(heightText.Text);
+        }
+
+        private void weightText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if (weightText.Text == "")
+            //    WeightSlider.Value = WeightSlider.Minimum;
+            //else
+            //    WeightSlider.Value = Convert.ToInt32(weightText.Text);
+
+        }
+
+       
+    }
+}
---
+++ BMI Calculator/BMI Calculator/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BMI_Calculator
+{
+    internal class MainPageViewModel : INotifyPropertyChanged
+    {
+        private double height = 180;
+        private double weight = 72;
+        private const double STEP = 1.0;
+        public double Height { 
+            get => height;
+            set
+            { 
+                height = NextStep(value);
+                RaisePropertyChanged(nameof(Bmi));
+                RaisePropertyChanged(nameof(Classification));
+            }
+        }
+
+        public double Weight
+        {
+            get => weight;
+            set
+            {
+                weight = NextStep(value);
+                RaisePropertyChanged(nameof(Bmi));
+                RaisePropertyChanged(nameof(Classification));
+            }
+        }
+
+        public double Bmi => Math.Round(Weight / Math.Pow(Height/100,2),2);
+
+        private double NextStep(double value) => Math.Round(value / STEP) * STEP;
+        
+        public string Classification
+        {
+            get
+            {
+                if (Bmi < 18.5)
+                    return "You are underweight";
+                else if (Bmi < 25)
+                    return "You are normal weight";
+                else if (Bmi < 30)
+                    return "You are overweight";
+                else
+                    return "You are obese";
+
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
